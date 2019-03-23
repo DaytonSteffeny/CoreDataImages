f32bbbf79ff305a56e80c2c56bef2b9110ec1dc0
Dayton <dayton@daytons-macbook-pro.local>
2019-03-22 21:27:17 -0500

    Initial Commit

---
+++ CoreDataImages/CoreDataImages.swift
@@ -0,0 +1,64 @@
+//
+//  Image+CoreDataClass.swift
+//  CoreDataImages
+//
+//  Created by Dayton on 3/22/19.
+//  Copyright © 2019 Dayton Steffeny. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+@objc(Image)
+public class Image: NSManagedObject {
+    var dateModified: Date? {
+        get {
+            return rawDateModified as Date?
+        }
+        set {
+            rawDateModified = newValue as NSDate?
+        }
+    }
+    
+    var size: Int? {
+        get {
+            return rawImage?.length
+        }
+    }
+    
+    var image: UIImage? {
+        get {
+            if let data = rawImage as Data? {
+                return UIImage(data: data)
+            }
+            
+            return nil
+        }
+        set(newImage) {
+            if let newImage = newImage {
+                if let data = newImage.pngData() as NSData? {
+                    print(data.length)
+                    rawImage = data
+                }
+            }
+            else {
+                rawImage = nil
+            }
+        }
+    }
+    
+    convenience init?(title: String?, image: UIImage?) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return nil
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        self.init(entity: Image.entity(), insertInto: managedContext)
+        
+        self.title = title
+        self.image = image
+        self.dateModified = Date()
+    }
+}
